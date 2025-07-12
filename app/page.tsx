6eb49382e68aaa88c5db20519a56ab4a72757042
RishabhHM <rishabh.hm06@gmail.com>
2025-07-12 02:29:48 -0400

    💄 style : Reduce hero section white space

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col w-full">
       {/* Hero Section */}
-      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <section className="w-full py-8 md:py-16 lg:py-20 xl:py-24">
         <div className="container px-4 md:px-6 mx-auto">
           <div className="grid gap-6 lg:grid-cols-3 lg:gap-12 xl:grid-cols-3">
             <div className="flex flex-col justify-center space-y-4 lg:col-span-1">
