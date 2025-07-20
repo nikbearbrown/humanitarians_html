53539fda4991fd1b548c2420b8bbce9ac986d7a4
Nik Bear Brown <nikbearbrown@users.noreply.github.com>
2025-07-20 18:41:18 -0400

    Update page.tsx

--- app/lyrical-literacy/page.tsx
+++ app/lyrical-literacy/page.tsx
@@ -22,7 +22,7 @@ export default function LyricalLiteracyPage() {
           <div className="relative w-full aspect-video rounded-lg overflow-hidden mb-8">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/videoseries?si=EXHOsuACAVlICiu3&amp;list=PLgOGgHS58rB8TP77w79PdmqF6qAAILkDP"
+              src="https://www.youtube.com/embed/juYjI3cncug?si=WqvsOj98AZuo_B-V"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
