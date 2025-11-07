50e22f975cfeb92b1862c0e9f70eb306cc1452e3
Nik Bear Brown <nikbearbrown@users.noreply.github.com>
2025-11-06 23:32:58 -0500

    Update page.tsx

--- app/lullabize-research/page.tsx
+++ app/lullabize-research/page.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import type { Metadata } from "next";
 import Link from "next/link";
-import Script from "next/script"; // Add this line
+import Script from "next/script";
 import PrimaryButton from "@/components/ui/primary-button";
 import SecondaryButton from "@/components/ui/secondary-button";
 import VideoThumbnail from "@/components/VideoThumbnail/VideoThumbnail";
