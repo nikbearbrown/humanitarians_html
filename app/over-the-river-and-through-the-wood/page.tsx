b3eae4c661d67e156d2676a7e742c0e08b27af6b
Nik Bear Brown <nikbearbrown@users.noreply.github.com>
2025-11-19 16:26:17 -0500

    Update page.tsx

--- app/over-the-river-and-through-the-wood/page.tsx
+++ app/over-the-river-and-through-the-wood/page.tsx
@@ -25,7 +25,7 @@ export default function OverTheRiverAndThroughTheWood() {
           <div className="relative w-full aspect-video rounded-lg overflow-hidden mb-8">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/VAfooxj1sbc?si=ZXtisZhvLLHRVgKo"
+              src="https://www.youtube.com/embed/VAfooxj1sbc?si=ZXtisZhvLLHRVgKo&enablejsapi=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
