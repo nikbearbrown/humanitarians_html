9c0b4b7c8a26ff0cedd76dd59638d16f2c6f999f
Nik Bear Brown <nikbearbrown@users.noreply.github.com>
2025-11-19 17:07:13 -0500

    Update page.tsx

--- app/raman-effect/page.tsx
+++ app/raman-effect/page.tsx
@@ -35,7 +35,7 @@ export default function RAMANEffectPage() {
             <div className="relative w-full aspect-video rounded-lg overflow-hidden mb-8">
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/3DJzPobJ6V8?si=i6tq8Kr1ZJvIWmpI"
+                src="https://www.youtube.com/embed/3DJzPobJ6V8?si=i6tq8Kr1ZJvIWmpI?enablejsapi=1"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
