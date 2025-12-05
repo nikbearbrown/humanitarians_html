d64f891de4d6c6ad50757609886218dcb83f7079
Nik Bear Brown <nikbearbrown@users.noreply.github.com>
2025-12-05 17:31:20 -0500

    Update page.tsx

--- app/muzak/page.tsx
+++ app/muzak/page.tsx
@@ -21,7 +21,7 @@ export default function Muzak() {
           <div className="relative w-full aspect-video rounded-lg overflow-hidden mb-8">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/videoseries?si=caS7A9UMCFewuuRB&amp;list=PLgOGgHS58rB-sBjm4oEfMfFXcYZf89IDo?enablejsapi=1"
+              src="https://www.youtube.com/embed/7aSfwv-ovcc?si=DJKRipqUw7XFhGQ9?enablejsapi=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
